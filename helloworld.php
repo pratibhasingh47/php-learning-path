9e95292e91337e48619031a690cf113aba201f87
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-08 00:58:01 +0530

    first commit

---
+++ helloworld.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <?php
+    // Comments
+    echo "Hello World";
+    if(true){
+        echo "Learning php !...";
+    }
+    ?>
+    <p>This is a paragraph !</p>
+</body>
+</html>
