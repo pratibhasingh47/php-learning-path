22a46526792256fd438b68029b8e47e403a23b80
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-08 00:59:28 +0530

    update

---
+++ variable.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $name = "Pratibha Singh";
+        $test = $name;
+        echo $name;
+
+        // Scaler types(contain one value)
+        $string = "Hello PHP";
+        $int = 4747;
+        $float = 20.61;
+        $bool = false;
+
+        // Array
+        $array = array("A","B","C","D");
+
+        // By defalut values
+        $names;
+        $string = '';
+        $int = 0;
+        $float =0;
+        $bool = false;
+
+    ?>
+
+    <p>Hello ! My name is <?php echo $name?> & I am learning php</p>
+</body>
+</html>
