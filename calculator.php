2074fad4cd5d00895af1999a24dfdcf8b7ca57a6
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-09 00:51:34 +0530

    update.php

--- calculator.php
+++ calculator.php
@@ -20,6 +20,7 @@
     </form>
 
     <?php
+    
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             $num01 = filter_input(INPUT_POST,"num01",FILTER_SANITIZE_NUMBER_FLOAT);
             $num02 = filter_input(INPUT_POST,"num02",FILTER_SANITIZE_NUMBER_FLOAT);
