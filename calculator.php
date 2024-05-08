c8b80b2f48e3712938c1f89801a872ccb2da6d9a
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-09 00:53:34 +0530

    update.php

--- calculator.php
+++ calculator.php
@@ -20,12 +20,11 @@
     </form>
 
     <?php
-    
+
         if($_SERVER["REQUEST_METHOD"] == "POST"){
             $num01 = filter_input(INPUT_POST,"num01",FILTER_SANITIZE_NUMBER_FLOAT);
             $num02 = filter_input(INPUT_POST,"num02",FILTER_SANITIZE_NUMBER_FLOAT);
             $operator = htmlspecialchars($_POST["operator"]);
-
 
             $errors = false;
 
