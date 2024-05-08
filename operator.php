3766e209f0dee95b35a996c62565dc49a1cb4cf7
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-09 00:48:00 +0530

    update.php

--- operator.php
+++ operator.php
@@ -14,6 +14,7 @@
         echo $c;
         echo "<br>";
 
+
         echo 1 + 2;
         echo "<br>";
         echo 10 % 4;
@@ -25,11 +26,13 @@
         echo (1 + 2) *4;
         echo "<br>";
 
+
         $x = 2;
         $x = $x +7;
         $x += 5;
         echo $x;
         echo "<br>";
+
 
         $w = 2;
         $p = 4;
@@ -37,6 +40,7 @@
             echo "This statement is true";
             echo "<br>";
         };
+        
 
         $w = 4;
         $p = 4;
