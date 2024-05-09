992b22961381afafad6f8602d5026c94fe11daf6
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-10 01:19:14 +0530

    update.php

---
+++ constant.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+    define("PI",3.14);
+    echo PI;
+    echo "<br>";
+
+    define("NAME","Pratibha Singh");
+    echo NAME;
+    echo "<br>";
+
+    function test (){
+        echo PI;
+        echo "<br>";
+        echo NAME;
+    }
+    test();
+
+    ?>
+</body>
+</html>
