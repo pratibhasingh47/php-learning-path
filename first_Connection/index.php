96771e4ee9e5551a701976a5a41cd8c2fca25c8f
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-12 00:22:45 +0530

    php

--- first_Connection/index.php
+++ first_Connection/index.php
@@ -9,10 +9,13 @@
     <h3>Sign-Up</h3>
 
     <form action="includes/formhandler.inc.php" method="post">
+
         <input type="text" name="username" placeholder="Username">
         <input type="text" name="email" placeholder="E-Mail">
         <input type="text" name="pwd" placeholder="Password">
+        
         <button>Submit</button>
     </form>
+
 </body>
 </html>
