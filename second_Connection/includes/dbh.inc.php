f033398cd97a8bbc1d3c6786dc7fdc6ae6850059
pratibhasingh_47 <pratibhasingh1624@gmail.com>
2024-05-12 00:25:44 +0530

    update.php

--- second_Connection/includes/dbh.inc.php
+++ second_Connection/includes/dbh.inc.php
@@ -5,10 +5,12 @@ $dsn = "mysql:host=localhost;dbname=myfirst_php_databse";
 $dbusername = "root";
 $dbpassword = "";
 
-try{
+try
+{
     $pdo = new PDO($dsn , $dbusername , $dbpassword);
     $pdo ->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 }
-catch(PDOException $e){
+catch(PDOException $e)
+{
     echo "Connection failed : " . $e->getMessage();
 }
